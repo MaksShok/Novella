9cd8dfebaca64240a6b59bfec397b178cb4bfc85
Mm <maxkulezenin@gmail.com>
2025-08-05 22:08:26 +0700

    Add mouseClickable feature

---
+++ Assets/_Project/Scripts/Misc/MouseClickable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Project.Scripts.Misc
+{
+    public class MouseClickable : MonoBehaviour
+    {
+        [SerializeField] 
+        private Collider2D _targetCollider;
+        
+        [SerializeField] 
+        private UnityEvent onClick;
+
+        private Camera sceneCamera;
+
+        private void Awake()
+        {
+            sceneCamera = Camera.main;
+            if (!sceneCamera)
+                Debug.LogError("Не найдена камера с тегом 'MainCamera'");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                CheckClick();
+        }
+
+        private void CheckClick()
+        {
+            Vector3 mousePos = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null && hit.collider == _targetCollider)
+            {
+                onClick?.Invoke();
+            }
+        }
+    }
+}
